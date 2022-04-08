7c07b2e72024c767b4945552f101b6d63dccf6fd
Joseph-Wairimu <josephkamore084@gmail.com>
2022-04-08 21:22:01 +0300

    register profile to admin

--- rate/models.py
+++ rate/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+import cloudinary
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile',null=True)
+    image = CloudinaryField('image')
     bio = models.TextField(max_length=255)
-    location = models.CharField(max_length=255)
     job_title = models.CharField(max_length=255)
-    image = models.ImageField(upload_to='images/')
-   
+    dateupdated= models.DateField(auto_now_add=True )
 
     def __str__(self):
         return self.name
