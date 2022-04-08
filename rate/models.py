bba056e903c94419253eb583c28fbabf486ea459
Joseph-Wairimu <josephkamore084@gmail.com>
2022-04-08 21:11:00 +0300

    profile class

--- rate/models.py
+++ rate/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile',null=True)
+    bio = models.TextField(max_length=255)
+    location = models.CharField(max_length=255)
+    job_title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='images/')
+   
+
+    def __str__(self):
+        return self.name
