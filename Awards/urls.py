4de8d006f4df93cb19eaedb7925c14eb0cd92ed9
Joseph-Wairimu <josephkamore084@gmail.com>
2022-04-08 19:58:00 +0300

    user registration form

--- Awards/urls.py
+++ Awards/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
+from rate import views as v
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('rate.urls')),
+     path('register/', v.register, name='register'),
+
 ]
--- rate/views.py
+++ rate/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+
+def register(response):
+    if response.method == "POST":
+        form = RegisterForm(response.POST)
+        if form.is_valid():
+
+           
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password1']
+            form.save()
+           
+            
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    return render(response, 'register/register.html', {'form': form})
