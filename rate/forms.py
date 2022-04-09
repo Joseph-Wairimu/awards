7579935ba87492ae8ab12612bcc6224d43ee8f73
Joseph-Wairimu <josephkamore084@gmail.com>
2022-04-09 10:06:46 +0300

    view function for profile edit

--- rate/forms.py
+++ rate/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
-
+from .models import Profile
 
 class RegisterForm(UserCreationForm):
     email= forms.EmailField()
@@ -10,3 +10,9 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model= User
         fields=["username","email","password1","password2"]
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['user','image','job_title','bio']        
--- rate/views.py
+++ rate/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import RegisterForm
+from .forms import RegisterForm, ProfileUpdateForm
 from .models import  Profile
 # Create your views here.
 
@@ -27,4 +27,15 @@ def profile(request):
         'profile': profile
     }
     return render(request, 'profile.html', context)
-   
+
+def edit_profile(request):
+    current_user = request.user
+    profile = Profile.objects.filter(user=current_user).first()
+    if request.method == 'POST':
+        form =  ProfileUpdateForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = ProfileUpdateForm(instance=profile)
+    return render(request, 'edit_profile.html', {'form': form})   
