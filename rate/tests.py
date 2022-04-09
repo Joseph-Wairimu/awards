997554d489d030d788b96e26a8a1923397cb5c20
Joseph-Wairimu <josephkamore084@gmail.com>
2022-04-09 20:39:23 +0300

    test case for our profile

--- rate/tests.py
+++ rate/tests.py
@@ -36,4 +36,30 @@ class ProjectTestClass(TestCase):
         self.project.save_project()
         project = Project.objects.filter(title__icontains='Test')
            
-    
+class ProfileTestClass(TestCase):
+    # Set up method
+    def setUp(self):
+        self.user = User.objects.create_user('testuser')
+
+        self.profile = Profile(bio='Test Bio', job_title='Test Job Title', user=self.user)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile, Profile))
+
+    def test_save_method(self):
+        self.profile.save_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) > 0)
+
+    def test_delete_method(self):
+        self.profile.save_profile()
+        profile = Profile.objects.filter(bio='Test Bio').delete()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) == 0)
+
+    def test_update_method(self):
+        self.profile.save_profile()
+        profile = Profile.objects.filter(bio='Test Bio').update(bio='Test Bio Updated')
+        profiles = Profile.objects.filter(bio='Test Bio Updated')
+        self.assertTrue(len(profiles) > 0)
+   
